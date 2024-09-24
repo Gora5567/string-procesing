63cf48c399f8c5e9976d3e4b444edda626cb2794
egormarik <egormarikpt@gmail.com>
2024-09-24 19:17:42 +0300

    Refactored project name Refactored directory name Added helper.py Changed README.md Renamed main.py to string-processing-hw.py Changed string-processing-hw.py

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# string-procesing
+# @author Yehor Marik This is a homework for the task about string processing
+# 1) Create variables, place the values in them - first name,last name and age.
+# Display the following sentence: "Hi! My name is first name last name, I'm age years old."
+#
+# 2) Display the lyrics to the children's song:
+#
+# Baa, baa, black sheep,
+#
+# Have you any wool?
+#
+# Yes sir, yes sir,
+#
+# Three bags full
+#
+#
+#
+# One for the master,
+#
+# One for the lady,
+#
+# And one for the little boy
+#
+# Who lives down the lane
---
+++ string-processing-hw.py
@@ -0,0 +1,28 @@
+import helper
+
+helper.start_task(1)
+
+name = "Yehor"
+surname = "Marik"
+age = "12"
+print(f"Hi! My name is {name} {surname}, I am {age} years old.")
+
+helper.end_task(1)
+
+
+helper.start_task(2)
+
+print("Baa, baa, black sheep,\n\n"
+      "Have you any wool?\n\n"
+      "Yes sir, yes sir,\n\n"
+      "Three bags full\n\n\n"
+      "One for the master,\n\n"
+      "One for the dame,\n\n"
+      "And one for the little boy\n\n"
+      "Who lives down the lane\n\n\n"
+      "Baa, baa, black sheep,\n\n"
+      "Have you any wool?\n\n"
+      "Yes sir, yes sir,\n\n"
+      "Three bags full")
+
+helper.end_task(2)
